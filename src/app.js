38a26913f8738bd3b4045d696df3690abc554007
Alberth-Std <202206259@est.umss.edu>
2025-12-11 15:31:26 -0400

    Correcion de HTTP/2 e implementacion correcta con Spdy

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-
-const connectDB = require('./config/db');
-
+const routes = require('./routes');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
-
-app.get('/', (req, res) => {
-  res.json({ message: 'API de Cuestionarios funcionando.' });
-});
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,45 +1,29 @@
 require("dotenv").config();
 const fs = require("fs");
+const spdy = require("spdy");
 const http = require("http");
 const https = require("https");
-const http2 = require("http2");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db"); 
 const app = require("./app");
 
-// Conectar a MongoDB
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB conectado"))
-  .catch(err => console.error("Error MongoDB:", err));
+// Conectar a MongoDB desde la carpeta config
+connectDB();
 
+// Leer certificados SSL
 const key = fs.readFileSync("cert/server.key");
 const cert = fs.readFileSync("cert/server.cert");
 
-// HTTP
+// Servidor HTTP
 http.createServer(app).listen(3000, () => {
-  console.log("Servidor HTTP → http://localhost:3000");
+  console.log("Servidor HTTP escuchando en http://localhost:3000");
 });
 
-// HTTPS
+// Servidor HTTPS
 https.createServer({ key, cert }, app).listen(3443, () => {
-  console.log("Servidor HTTPS → https://localhost:3443");
+  console.log("Servidor HTTPS escuchando en https://localhost:3443");
 });
 
-
-// HTTP/2
-const http2Server = http2.createSecureServer({ key, cert });
-
-http2Server.on("stream", (stream, headers) => {
-  // Respuesta simple compatible con HTTP/2
-  stream.respond({
-    "content-type": "application/json",
-    ":status": 200
-  });
-
-  stream.end(JSON.stringify({
-    message: "HTTP/2 funcionando"
-  }));
-});
-
-http2Server.listen(4000, () => {
-  console.log("Servidor HTTP/2 → https://localhost:400");
+// Servidor HTTP/2
+spdy.createServer({ key, cert }, app).listen(4000, () => {
+  console.log("Servidor SPDY/HTTP2 escuchando en https://localhost:4000");
 });
